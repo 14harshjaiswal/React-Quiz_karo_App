4a095e8f23b1864eea2933ef185db794bf29145b
Harsh <harshjaiswal960@gmail.com>
2021-06-23 18:05:35 +0530

    initial commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,21 @@
+const Footer = () => {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          marginBottom: 10,
+        }}
+      >
+        Made with ♥ by{" "}
+        <a
+          href="https://github.com/14harshjaiswal"
+          style={{ cursor: "pointer" }}
+        >
+          Harsh Jaiswal
+        </a>
+      </div>
+    );
+  };
+  
+  export default Footer;
+  
